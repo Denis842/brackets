73b244db3174847977eb06e69cd97f625919f0c0
Denis842 <denis.gusachek@gmail.com>
2019-10-10 22:37:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let i = 0;
+  let brackets;
+
+  for (let i = 0; i < bracketsConfig.length;) {
+    
+    brackets = bracketsConfig[i].join('');
+
+    if (str.includes(brackets)) {
+      str = str.replace(brackets, '');
+      i = 0;
+    }
+    else {
+      i++;
+    }
+  }
+
+  if (str == ''){
+    result = true;
+  }
+  else {
+      result = false;
+  }
+  
+  return result;
+};
+
